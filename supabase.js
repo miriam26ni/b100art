3780c7842d529aa80df0435253d89028dc34e462
miriam26ni <miriam26ni@gmail.com>
2025-12-04 17:46:46 +0000

    Actualiza URL y ANON KEY de Supabase

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = 'https://jkiwfvolipgttkyxcndc.supabase.co'  // ← cambia por tu URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpraXdmdm9saXBndHRreXhjbmRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNzIyODksImV4cCI6MjA3OTg0ODI4OX0.lBGeIM4w6Gzy3dopkZMMAGr3_9uL1JHRRxq5URUhZEU'             // ← cambia por tu anon key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  "https://vfnouwdawgkrtexudfkj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmbm91d2Rhd2drcnRleHVkZmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MjQ5MzEsImV4cCI6MjA4MDMwMDkzMX0.yd3odCqxNeeMGa1SbMCQrNUPzIenQaJCgu_CG8wIhJA"
+);
